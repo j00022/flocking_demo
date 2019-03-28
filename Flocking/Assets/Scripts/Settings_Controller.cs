9d84f1d42479c6a4c11fc9519255f4f3e9d9daf7
JackC <jackc0022@gmail.com>
2019-03-28 16:52:16 -0700

    Reorganized files

---
+++ Flocking/Assets/Scripts/Settings_Controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings_Controller : MonoBehaviour {
+
+    GameObject settings;
+
+	// Use this for initialization
+	void Start () {
+        settings = GameObject.FindWithTag("SettingsUI");
+        settings.SetActive(false);
+
+    }
+	
+	public void Toggle_settings() {
+        settings.SetActive(!settings.activeSelf);
+    }
+
+
+}
