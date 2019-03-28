f8fcc16ae39c8a4df78bcc44e563998f17f5364d
JackC <jackc0022@gmail.com>
2019-03-28 15:39:05 -0700

    Initial push & created basis of settings UI

---
+++ Assets/Scripts/Settings_Controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings_Controller : MonoBehaviour {
+
+    public GameObject settings;
+
+	// Use this for initialization
+	void Start () {
+        if (settings == null) {
+            settings = GameObject.FindWithTag("SettingsUI");
+            settings.SetActive(false);
+        }
+    }
+	
+	public void Toggle_settings() {
+        settings.SetActive(!settings.activeSelf);
+    }
+}
